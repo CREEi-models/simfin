e1115033ae5509e02e7c92bc975225602f4c3ca2
Julien Navaux <julien.navaux@hec.ca>
2021-03-23 12:07:54 -0400

    update docs

--- simfin/federal/other_transfers.py
+++ simfin/federal/other_transfers.py
@@ -6,13 +6,23 @@ class other_transfers(account):
 
     Parameters
     ----------
+    iprice: boolean
+        Switch pour intégrer ou non la croissance du niveau général des prix.
     igdp: boolean
-        Switch pour intégrer ou non la croissance du PIB.
+        Switch pour intégrer ou non la croissance potentielle du PIB réel.
     ipop: boolean
         Switch pour intégrer ou non la croissance de la population.
     '''
+    def __init__(self,value,iprice=True,igdp=True,ipop=False,others=None):
+        self.value = value
+        self.iprice = iprice
+        self.igdp = igdp
+        self.ipop = ipop
+        return
     def grow(self,macro,pop,eco,others=None):
-        rate = 1.0 + macro.infl
+        rate = 1.0
+        if self.iprice:
+            rate += macro.infl
         if self.igdp:
             rate += macro.gr_Yp
         if self.ipop:
