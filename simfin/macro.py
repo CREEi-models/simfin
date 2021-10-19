3dc30bf9583f501ad629e97bf7f2d65df53a23a4
Nicholas-James Clavet <nicholas-james.clavet@hec.ca>
2021-10-19 09:09:05 -0400

    Adding labor market output & updating growth func
    New outputs are employment and hours worked. Function grow_infl and grow_tfp has been added.

--- simfin/macro.py
+++ simfin/macro.py
@@ -22,7 +22,7 @@ class macro:
         self.E_w = 0 # Sur quel agrégat.
         self.C_hh = self.data.loc['C_hh',1]
         self.gr_K = self.g_pars.loc['g_real_K',1]
-        self.gr_A = self.gr.loc['gr_A',2021]
+        self.gr_A = 0.0
         self.infl = 0.02
         self.gr_L = 0.0
         self.gr_Lp = 0.0
@@ -38,25 +38,26 @@ class macro:
         self.N = self.data.loc['N',1]
         self.C_hh = self.data.loc['C_hh',1]
         self.gr_K = self.g_pars.loc['g_real_K',1]
-        self.gr_A = self.gr.loc['gr_A',2021]
+        self.gr_A = 0.0
         self.gr_L = 0.0
         self.gr_N = 0.0
         self.gr_Y = 0.0
         self.year = self.start_yr
         self.set_align_emp(pop,eco)
-    def set_rate_real_K(self,rate=None):
-        if rate!=None:
-            self.gr_K = rate
+    def grow_tfp(self,year):
+        self.gr_A=self.gr_A
         return
-    def set_rate_TFP(self,rate=None):
-        if rate!=None:
-            self.gr.loc['gr_A',self.year]
+    def grow_infl(self,year):
+        self.infl=self.infl
         return
     def set_align_emp(self,pop,eco):
         wages = eco['earn_c']/eco['hours_c']
         self.eff_hours = wages/ wages.mean()
         L = pop.multiply(eco['emp']*eco['hours_c']*self.eff_hours,fill_value=0.0).sum()
         self.align = self.L/L
+        self.employment = pop.multiply(eco['emp'],fill_value=0.0).sum()
+        self.employment_25_54 = pop.multiply(eco.query('age >= 25 and age <= 54')['emp'],fill_value=0.0).sum()
+        self.hours = pop.multiply(eco['hours_c'],fill_value=0.0).mean() * self.align
         return
     def set_align_cons(self,pop,eco):
         C = (pop.multiply(eco['cons'],fill_value=0.0).sum())
@@ -67,6 +68,9 @@ class macro:
         L = pop.multiply(eco['emp']*eco['hours_c']*self.eff_hours,fill_value=0.0).sum() * self.align
         self.gr_L = L/ self.L-1
         self.L = L
+        self.employment = pop.multiply(eco['emp'],fill_value=0.0).sum()
+        self.employment_25_54 = pop.multiply(eco.query('age >= 25 and age <= 54')['emp'],fill_value=0.0).sum()
+        self.hours = pop.multiply(eco['hours_c'],fill_value=0.0).mean() * self.align
         Lp = pop.multiply(eco['emp']*eco['hours_c']*self.eff_hours,fill_value=0.0).sum() * self.align
         self.gr_Lp = Lp/ self.Lp-1
         self.Lp = Lp
@@ -100,7 +104,7 @@ class macro:
         igra = True
         rate = 1.0+self.infl
         if igra == True:
-            rate += 1/self.g_pars.loc['alpha_L',1]* self.gr.loc['gr_A',self.year] # Part TFP non lié à l'éducation (50%)
+            rate += 1/self.g_pars.loc['alpha_L',1]*self.gr.loc['gr_A',self.year] # Part TFP non lié à l'éducation (50%)
         eco['earn_c'] = eco['earn_c']*rate
     def non_work_earnings(self,pop,eco): # B. Achou, pas sûr que cette fonction soit nécessaire
         E_wno = pop.multiply(eco['taxinc'],fill_value=0.0).sum()
@@ -114,7 +118,7 @@ class macro:
         igra = True
         rate = 1.0+self.infl
         if igra == True:
-            rate += 1/self.g_pars.loc['alpha_L',1]* self.gr.loc['gr_A',self.year]  # Part TFP non lié à l'éducation (50%)
+            rate += 1/self.g_pars.loc['alpha_L',1]*self.gr.loc['gr_A',self.year]  # Part TFP non lié à l'éducation (50%)
         eco['taxinc'] = eco['taxinc']*rate
     def pop(self,pop):
         N = pop.sum()
